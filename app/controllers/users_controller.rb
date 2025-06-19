eda98d233dc48d4b88891c2c910757279b267965
Noah Fonseca <nfonseca@rrc.ca>
2025-06-19 12:21:08 -0500

    Added Text Search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
-  def index
-    @states = State.includes(:country).all
-    @selected_state = params[:state_id]
+def index
+  @states = State.includes(:country).all
+  @selected_state = params[:state_id]
+  @query = params[:query]
 
-    @users = User.includes(state: :country)
-    @users = @users.where(state_id: @selected_state) if @selected_state.present?
-  end
+  @users = User.includes(state: :country)
+  @users = @users.where(state_id: @selected_state) if @selected_state.present?
+  @users = @users
+  .joins(state: :country)
+  .where("users.first_name LIKE :q OR users.last_name LIKE :q OR users.email LIKE :q OR users.phone_number LIKE :q OR states.name LIKE :q OR countries.name LIKE :q",
+    q: "%#{@query}%"
+  ) if @query.present?
+end
 end
