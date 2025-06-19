f979df3c75a7a5e0a6f2174e4ff7d24865b72694
Noah Fonseca <nfonseca@rrc.ca>
2025-06-19 14:12:35 -0500

    Added About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "pages/about"
   get "users/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,5 +13,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+ resources :pages
  resources :users
 end
